2742c8185907b9778b0285ac6b37ec7df3efd435
Mohamed Abouzid <mohamed.abouzid@bioinfsys.uni-giessen.de>
2025-11-13 00:08:41 +0100

    add CRUD

--- product_service/src/main/java/com/example/product_service/service/ProductService.java
+++ product_service/src/main/java/com/example/product_service/service/ProductService.java
@@ -1,4 +1,62 @@
 package com.example.product_service.service;
 
+import com.example.product_service.entity.Product;
+import com.example.product_service.repository.ProductRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Service
+@Transactional
 public class ProductService {
+    private final ProductRepository repo;
+
+    public ProductService(ProductRepository repo) {
+        this.repo = repo;
+    }
+    // create
+    public Product create(Product product) {
+        validateProduct(product);
+        return repo.save(product);
+    }
+    // read all
+    public List<Product> findAll() {
+        return repo.findAll();
+    }
+    // read by id
+    public Product findById(Long id) {
+        return repo.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Product not found with id: " + id));
+    }
+    // update
+    public Product update(Long id, Product updated) {
+        Product existing = findById(id);
+        validateProduct(updated);
+
+        existing.setName(updated.getName());
+        existing.setDescription(updated.getDescription());
+        existing.setPrice(updated.getPrice());
+        existing.setStock(updated.getStock());
+
+        return repo.save(existing);
+    }
+    //delete
+    public void delete(Long id) {
+        if (!repo.existsById(id)) {
+            throw new IllegalArgumentException("Cannot delete. Product not found with id: " + id);
+        }
+        repo.deleteById(id);
+    }
+    // business validation
+    private void validateProduct(Product product) {
+        if (product.getPrice() == null || product.getPrice().compareTo(BigDecimal.ZERO) <= 0) {
+            throw new IllegalArgumentException("Price must be greater than 0");
+        }
+        if (product.getStock() == null || product.getStock() < 0) {
+            throw new IllegalArgumentException("Stock cannot be negative");
+        }
+    }
 }
+
--- product_service/src/main/java/com/example/product_service/web/ProductController.java
+++ product_service/src/main/java/com/example/product_service/web/ProductController.java
@@ -2,6 +2,7 @@ package com.example.product_service.web;
 
 import com.example.product_service.entity.Product;
 import com.example.product_service.repository.ProductRepository;
+import com.example.product_service.service.ProductService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -9,17 +10,30 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/products")
 public class ProductController {
-    private final ProductRepository repository;
+    private final ProductService service;
 
-    public ProductController(ProductRepository repository) {
-        this.repository = repository;
+    public ProductController(ProductService service) {
+        this.service = service;
     }
     @GetMapping
     public List<Product> getAll() {
-        return repository.findAll();
+        return service.findAll();
+    }
+    @GetMapping("/{id}")
+    public Product getById(@PathVariable Long id) {
+        return service.findById(id);
     }
     @PostMapping
     public Product create(@RequestBody Product product) {
-        return repository.save(product);
+        return service.create(product);
+    }
+    // POST update
+    @PostMapping("/{id}")
+    public Product update(@PathVariable Long id,@RequestBody Product product) {
+        return service.update(id, product);
+    }
+    @DeleteMapping("/{id}")
+    public void  delete(@PathVariable Long id) {
+        service.delete(id);
     }
 }
